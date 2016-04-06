3900c04621ebfe3e44aa8745f7c639a3623574fd
Jack Li <drjackl@gmail.com>
2016-04-05 23:15:48 -0700

    New blank project (single view application)

---
+++ Q&A/Q&A/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Q&A
+//
+//  Created by Jack Li on 4/5/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
