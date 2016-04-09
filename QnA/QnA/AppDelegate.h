0c7653f3ee78e425d1bc49876fd6cf7085883dfd
Jack Li <drjackl@gmail.com>
2016-04-08 18:30:57 -0700

    Adding "new blank project again" this time

---
+++ QnA/QnA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QnA
+//
+//  Created by Jack Li on 4/8/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QnA/QnA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QnA
+//
+//  Created by Jack Li on 4/8/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
