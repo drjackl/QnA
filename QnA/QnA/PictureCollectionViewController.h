6035e0d6651672cd53e5a6e733d344790eb1c886
Jack Li <drjackl@gmail.com>
2016-04-20 09:24:07 -0700

    Preliminary profile image picker added with PictureCollectionVC (just shows single test cell)

---
+++ QnA/QnA/PictureCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  PictureCollectionViewController.h
+//  QnA
+//
+//  Created by Jack Li on 4/19/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PictureCollectionViewController : UICollectionViewController
+
+@end
---
+++ QnA/QnA/PictureCollectionViewController.m
@@ -0,0 +1,102 @@
+//
+//  PictureCollectionViewController.m
+//  QnA
+//
+//  Created by Jack Li on 4/19/16.
+//  Copyright © 2016 Jack Li. All rights reserved.
+//
+
+#import "PictureCollectionViewController.h"
+
+@interface PictureCollectionViewController ()
+
+@end
+
+@implementation PictureCollectionViewController
+
+static NSString*const reuseIdentifier = @"picCell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // This is a mistake here. Do NOT register the class here if you have a storybaord cell
+    // Register cell classes
+    //[self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    
+    // Do any additional setup after loading the view.
+    
+    self.navigationItem.title = @"Test";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark <UICollectionViewDataSource>
+
+// default is 1 like in table?
+//- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+//#warning Incomplete implementation, return the number of sections
+//    return 1;
+//}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of items
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+}
+*/
+
+@end
